c18063b009cac186b4bab27109e776a8fb933789
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-04-20 15:40:19 +0300

    Реализовать компонент AboutProject #10

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
 import NavTab from "../NavTab/NavTab";
+import AboutProject from "../AboutProject/AboutProject";
 const Main = ({currentPage}) => {
     return (
         <>
             <Header currentPage={currentPage}/>
             <Promo />
             <NavTab />
+            <AboutProject />
         </>
     );
 }
