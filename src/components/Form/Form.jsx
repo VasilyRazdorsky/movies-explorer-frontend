5541e9b76311f7ed5fcc6089431f4ecb768f0297
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-04-24 15:52:14 +0300

    Вынести форму в отдельный универсальный компонент Form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,15 @@
+const Form = ({ currentPage, submitButtonText, children }) => {
+    return (
+        <form
+            action="#"
+            className="form"
+            onSubmit={console.log('submit login')}
+            noValidate
+        >
+            {children}
+            <button type="submit" className={`form__submit-button ${currentPage.pathname === "/signin" ? "form__submit-button_action_login" : "form__submit-button_action_register"}`}>{submitButtonText}</button>
+        </form>
+    );
+}
+
+export default Form;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Header from "../Header/Header";
+import Form from "../Form/Form";
 
 const Login = ({currentPage}) => {
     return (
@@ -7,34 +8,34 @@ const Login = ({currentPage}) => {
             <Header currentPage={currentPage} />
             <section className="login">
                 <h1 className="login__title">Рады видеть!</h1>
-                <form
-                action="#"
-                className="login__form"
-                onSubmit={console.log('submit login')}
-                >
-                    <label htmlFor="email" className="login__label">E-mail</label>
+                
+                <Form
+                    currentPage={currentPage}
+                    submitButtonText="Войти"
+                    children={
+                    <>
+                    <label htmlFor="email" className="form__label">E-mail</label>
                     <input 
-                      className="login__input"
-                      type="email"
-                      name="email"
-                      id="email"
-                      onChange={console.log("change email")}
-                      required
+                        className="form__input"
+                        type="email"
+                        name="email"
+                        id="email"
+                        onChange={console.log("change email")}
+                        required
                     />
-
-                    <label htmlFor="password" className="login__label">Пароль</label>
+        
+                    <label htmlFor="password" className="form__label">Пароль</label>
                     <input 
-                      className="login__input"
-                      type="password"
-                      name="password"
-                      id="password"
-                      onChange={console.log("change password")}
-                      required
-                    />
+                        className="form__input"
+                        type="password"
+                        name="password"
+                        id="password"
+                        onChange={console.log("change password")}
+                        required
+                     />
                     <span className="form__error">Что-то пошло не так...</span>
-
-                    <button type="submit" className="login__submit-button login__submit-button-action-login">Войти</button>
-                </form>
+                    </>
+                }/>
 
                 <p className="login__redirect-text">Ещё не зарегистрированы? <Link to="/signup" className="login__redirect-link">Регистрация</Link></p>
             </section>
