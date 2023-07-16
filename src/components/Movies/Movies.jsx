0e85855f8fbaa58985a29fdbc106866cd399ae73
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-07-16 12:04:40 +0300

    Реализовать компонент MoviesCardList

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,13 @@
 import Header from "../Header/Header";
 import Search from "../Search/Search";
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
 
 const Movies = ({ currentPage }) => {
     return ( 
         <>
             <Header currentPage={currentPage} />
             <Search />
+            <MoviesCardList />
         </>
         
     );
---
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -0,0 +1,25 @@
+export default function MoviesCardList() {
+    return <section className="movies-list">
+        <div className="movies-list__cards-container">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+        </div>
+
+        <button className="movies-list__more-button">Ещё</button>
+    </section>
+}
